e9295d565114d0bc013c2a740d2f631165f65d55
tapshyamangal567 <mangaltapasya@gmail.com>
2026-04-05 19:22:58 +0530

    Added real Google Drive ID for xgb_scaler.pkl

--- backend/build.sh
+++ backend/build.sh
@@ -21,7 +21,7 @@ gdown --id "1SkMzV9Qx3ob7AA9eDMDsuG0ru7A85yup" -O models/xgb_model.pkl
 # 3. Download gb_model.pkl (Note: Ensure this ID is correct, it was identical to lstm_model's ID in your paste)
 gdown --id "1Ot3qYZ2PPeV-3aWRwAE63aggwPi_vbXE" -O models/gb_model.pkl
 
-# 4. Download 4th model (Replace the ID and the output filename)
-gdown --id "https://drive.google.com/file/d/1MTlb7BuH6QNEcuwJPMsEFnNbIOYYl9n-/view?usp=sharing" -O models/lstm_model.pkl
+# 4. Download xgb_scaler.pkl
+gdown --id "1G6EmG2GqMzYzbT4lyGKbKLDIkkOc3WqU" -O models/xgb_scaler.pkl
 
 echo "Build and model download completed successfully!"
